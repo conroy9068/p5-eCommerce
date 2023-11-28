6a8fe78e941326944913682f51b156282be5fbf5
David <david@trave.io>
2023-11-28 23:15:40 +0000

    Bug images not displaying on product page

--- products/urls.py
+++ products/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -5,4 +7,4 @@ from . import views
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

10c7b3b4f3dd1af1b76bd0f0b6a46dbdf8bbefaf
David <david@trave.io>
2024-01-14 18:19:02 +0000

    Add Stripe API keys to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OYWOmF6Vc5JVoWZkc4OnevOTWvNEoy4fjzCd628fn394ja913tjMyfu8KgLWGFsZtXPeHGw0fipTKsJdEieZCDa00iEi4vtb4',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

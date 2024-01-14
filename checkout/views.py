71619261de2e5423edfe755dc7cf0e800b26d270
David <david@trave.io>
2024-01-14 20:58:43 +0000

    Add Stripe integration to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,5 +1,9 @@
+import stripe
+from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import redirect, render, reverse
+
+from bag.contexts import bag_contents
 
 from .forms import OrderForm
 
@@ -9,17 +13,35 @@ from .forms import OrderForm
 def checkout(request):
     """ A view to return the checkout page """
 
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OYWOmF6Vc5JVoWZkc4OnevOTWvNEoy4fjzCd628fn394ja913tjMyfu8KgLWGFsZtXPeHGw0fipTKsJdEieZCDa00iEi4vtb4',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)

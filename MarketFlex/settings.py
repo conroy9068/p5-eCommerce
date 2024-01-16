a30fcb3bf4c693f9f55f3d86ef821f7eea463c16
David <david@trave.io>
2024-01-16 22:16:31 +0000

    Update .gitignore and add loading overlay

--- MarketFlex/settings.py
+++ MarketFlex/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-yo73r7wc$!7s2s%n%7wzh2)s+wrk+ii+kzcsahbdoennmkm1ej'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

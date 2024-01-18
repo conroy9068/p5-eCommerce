d30fc45d7dcea5859ecc195ea912d8f2c0a560ad
David <david@trave.io>
2024-01-18 23:02:08 +0000

    Update profile template and views, and modify base.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import UserProfile
 
 # Create your views here.
 
@@ -6,8 +8,11 @@ from django.shortcuts import render
 def profile(request):
     """ Display the user's profile. """
 
+    profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'profiles/profile.html'
     context = {
+        'profile': profile,
     }
 
     return render(request, template, context)

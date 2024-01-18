097f4111668656185002fd1fd2ba9bce209e7f05
David <david@trave.io>
2024-01-18 23:53:52 +0000

    Update UserProfile model and profile.html template Add JS for countryfield dropdown

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, render
 
+from .forms import UserProfileForm
 from .models import UserProfile
 
 # Create your views here.
@@ -10,9 +12,20 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        orders: 'orders',
+        'on_profile_page': True
     }
 
     return render(request, template, context)

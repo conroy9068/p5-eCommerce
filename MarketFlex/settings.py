a44758ffb4dc113f678efd70fd81611d05e67585
David <david@trave.io>
2024-01-24 00:09:33 +0000

    Update SECRET_KEY and DEBUG settings

--- MarketFlex/settings.py
+++ MarketFlex/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yo73r7wc$!7s2s%n%7wzh2)s+wrk+ii+kzcsahbdoennmkm1ej'
+SECRET_KEY = os.getenv('SECRET_KEY','')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://p5-ecommerce-fb6cb413b539.herokuapp.com/','localhost']
 

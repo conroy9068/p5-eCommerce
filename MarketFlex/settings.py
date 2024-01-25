7d537e2030fee542c5ae4282045a526401ce361d
David <david@trave.io>
2024-01-25 21:11:38 +0000

    Update AWS storage bucket name

--- MarketFlex/settings.py
+++ MarketFlex/settings.py
@@ -185,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'p5-ecommerce-fb6cb413b539'
+    AWS_STORAGE_BUCKET_NAME = 'p5-ecommerce'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')

8a302402f4348ddadd145d2f383d8c1461375e0a
David <david@trave.io>
2024-01-25 00:02:32 +0000

    Update ALLOWED_HOSTS in settings.py

--- MarketFlex/settings.py
+++ MarketFlex/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY','')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://MarketFlex-fb6cb413b539.herokuapp.com/','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'p5-ecommerce-fb6cb413b539.herokuapp.com', 'https://p5-ecommerce-fb6cb413b539.herokuapp.com/']
 
 
 # Application definition

b1a2586b7c15ec1f6bca1b8335abfdfc2c73a557
David <david@trave.io>
2024-02-09 11:36:36 +0000

    Add Contact model.py and view.py def ContactCreateView

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    class Meta:
+        verbose_name_plural = 'Contacts'
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from djang.contrib import messages
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
+from django.shortcuts import render, reverse
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Contact
+
+
+class ContactCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Contact
+    fields = ['name', 'email', 'message']
+    success_message = "Your message has been sent successfully."
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, "Your message has been sent.")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('home')

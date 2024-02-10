ec701d1a75ff9094adecf94fbb7f436ef4eafac8
David <david@trave.io>
2024-02-10 01:27:54 +0000

    Update URLs and CSS styles

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-
 from .views import subscribe
 
 urlpatterns = [
-    path('subscribe/', subscribe, name='subscribe'),
+    path('', subscribe, name='subscribe'),
 ]

3995f1e0b921f60193e07b3d49255f7f9a684ada
David <david@trave.io>
2024-02-10 00:40:54 +0000

    Add Contact app model and ContactCreateView in views.py

--- contact/views.py
+++ contact/views.py
@@ -1,23 +1,31 @@
-from djang.contrib import messages
 from django.conf import settings
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from django.core.mail import send_mail
-from django.shortcuts import render, reverse
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
+from .forms import ContactForm
 from .models import Contact
 
 
-class ContactCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+class ContactCreateView(CreateView):
     model = Contact
-    fields = ['name', 'email', 'message']
+    form_class = ContactForm
+    template_name = 'contact/contact_form.html'
+    success_url = reverse_lazy('home')  # Update with your actual success URL
     success_message = "Your message has been sent successfully."
 
     def form_valid(self, form):
-        form.instance.author = self.request.user
-        messages.success(self.request, "Your message has been sent.")
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('home')
+        # Save the form first
+        self.object = form.save()
+        # Send an email
+        send_mail(
+            f"Message from {self.object.name}",
+            self.object.message,
+            self.object.email,
+            [settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+        messages.success(self.request, self.success_message)
+        return redirect(self.success_url)

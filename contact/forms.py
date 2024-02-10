d8d161d51ddbb514e4c960016281c272d7883ef7
David <david@trave.io>
2024-02-10 12:27:30 +0000

    Add form styling and helper to ContactForm

--- contact/forms.py
+++ contact/forms.py
@@ -7,3 +7,13 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.helper = forms.FormHelper()
+        self.helper.add_input(forms.Submit('submit', 'Send message'))
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['message'].widget.attrs.update({'class': 'form-control', 'rows': 5})
+        self.fields['message'].label = "Your message"
+        self.fields['message'].help_text = "Write your message here"

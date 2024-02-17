d3f7b5e53c3929e95289974238bf79d14f69def3
David <david@trave.io>
2024-02-17 13:24:01 +0000

    Add about page and corresponding view to enhance seo

--- home/views.py
+++ home/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     """A view that displays the index page"""
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """A view that displays the about page"""
+    return render(request, 'home/about.html')

b7a4c512863b2f390cb4b02ca9c44c04ffe8bda4
David <david@trave.io>
2024-02-22 07:57:46 +0000

    Update DEBUG setting and image file extension in about.html

--- MarketFlex/settings.py
+++ MarketFlex/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',
                  'p5-ecommerce-fb6cb413b539.herokuapp.com',
